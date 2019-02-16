7eca16c20cfc75d3df3f6f33438cbfc2dd7fe9fd
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-02-16 10:57:52 -0200

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,32 @@
+using GeoStatsBase
+using GeoStatsDevTools
 using SpectralGaussianSimulation
-using Test
+using Plots; gr()
+using VisualRegressionTests
+using Test, Pkg, Random
+
+# environment settings
+islinux = Sys.islinux()
+istravis = "TRAVIS" ∈ keys(ENV)
+datadir = joinpath(@__DIR__,"data")
+visualtests = !istravis || (istravis && islinux)
+if !istravis
+  Pkg.add("Gtk")
+  using Gtk
+end
 
 @testset "SpectralGaussianSimulation.jl" begin
-    # Write your own tests here.
+  if visualtests
+    problem = SimulationProblem(RegularGrid{Float64}(100,100), :z => Float64, 3)
+
+    Random.seed!(2019)
+    solver = SpecGaussSim(:z => (variogram=GaussianVariogram(range=10.),))
+    solution = solve(problem, solver)
+    @plottest plot(solution) joinpath(datadir,"isotropic.png") !istravis
+
+    Random.seed!(2019)
+    solver = SpecGaussSim(:z => (variogram=GaussianVariogram(distance=Ellipsoidal([20.,5.],[0.])),))
+    solution = solve(problem, solver)
+    @plottest plot(solution) joinpath(datadir,"anisotropic.png") !istravis
+  end
 end
